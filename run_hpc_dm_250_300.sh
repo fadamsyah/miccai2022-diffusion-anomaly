7a833e22046023bd8e071a1329f1bd34bdae3a44
fadamsyah <fadillahadam11@gmail.com>
2023-09-25 12:03:49 +0400

    Fix bugs

--- run_hpc_dm_250_300.sh
+++ run_hpc_dm_250_300.sh
@@ -6,7 +6,8 @@
 
 export MODEL_FLAGS="--image_size 256 --num_channels 128 --class_cond True --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
 export DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False"
-export CLASSIFIER_FLAGS="--image_size 256 --classifier_attention_resolutions 32,16,8 --classifier_depth 4 --classifier_width 32 --classifier_pool attention --classifier_resblock_updown True --classifier_use_s>export SAMPLE_FLAGS="--batch_size 1 --timestep_respacing ddim1000 --use_ddim True"
+export CLASSIFIER_FLAGS="--image_size 256 --classifier_attention_resolutions 32,16,8 --classifier_depth 4 --classifier_width 32 --classifier_pool attention --classifier_resblock_updown True --classifier_use_scale_shift_norm True"
+export SAMPLE_FLAGS="--batch_size 1 --timestep_respacing ddim1000 --use_ddim True"
 
 nvidia-smi
 
